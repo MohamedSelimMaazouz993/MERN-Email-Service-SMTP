083e600d1675d5bf0966249caaa8b90f01f173fa
Mohamed Selim Maazouz <71633887+MohamedSelimMaazouz993@users.noreply.github.com>
2023-05-02 20:45:03 +0100

    Update ContactForm.js

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Container, Form, Button, Alert } from 'react-bootstrap';
-
+import './ContactForm.css';
 const ContactForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -11,7 +11,7 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('/api/sendmail', { name, email, message })
+    axios.post('http://localhost:5000/api/sendmail', { name, email, message })
       .then((res) => {
         setName('');
         setEmail('');
@@ -27,21 +27,22 @@ const ContactForm = () => {
 
   return (
     <Container>
-      <h1>Contact Us</h1>
+      <h1>Complaint & Suggestion Box</h1>
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
+        <Form.Group controlId="formBasicName" className="mb-3" >
+          <Form.Label>Name    : </Form.Label>
           <Form.Control type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
         </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+        <Form.Group controlId="formBasicEmail" className="mb-3">
+          <Form.Label>Email address   :  </Form.Label>
           <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
-
-        <Form.Group controlId="formBasicMessage">
-          <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Enter your message" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <Form.Group controlId="formBasicMessage" className="mb-3">
+          <Form.Label>Message   :  </Form.Label>
+          <div className='message'>
+          <Form.Control as="textarea" rows={5} placeholder="Enter your message" value={message} onChange={(e) => setMessage(e.target.value)} />
+          </div>
         </Form.Group>
 
         <Button variant="primary" type="submit">
